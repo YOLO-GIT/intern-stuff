fe32c344510abd65bb44b95b8d9746005257195a
YOLO-GIT <ahmadsufi345@gmail.com>
2024-07-22 01:39:04 +0800

    Update the header with Router plugin

--- intern-stuff/src/components/header.jsx
+++ intern-stuff/src/components/header.jsx
@@ -1,48 +1,59 @@
 import { Avatar, Dropdown, Navbar } from "flowbite-react";
+import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import Logo from "../assets/logo.png";
 import React from "react";
 
 const Header = () => {
   return (
-    <Navbar fluid rounded>
-      <Navbar.Brand href="https://flowbite-react.com">
+    <Navbar fluid>
+      <Navbar.Brand href="https://github.com/YOLO-GIT/intern-stuff">
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-          Flowbite React
+          Praktikal Archive
         </span>
       </Navbar.Brand>
       <div className="flex md:order-2">
         <Dropdown
           arrowIcon={false}
           inline
-          label={
-            <Avatar
-              alt="User settings"
-              img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-              rounded
-            />
-          }
+          label={<Avatar alt="YOLO-GIT" img={Logo} rounded />}
         >
           <Dropdown.Header>
-            <span className="block text-sm">Bonnie Green</span>
+            <span className="block text-sm">YOLO-GIT</span>
             <span className="block truncate text-sm font-medium">
-              name@flowbite.com
+              gityolo7@gmail.com
             </span>
           </Dropdown.Header>
-          <Dropdown.Item>Dashboard</Dropdown.Item>
-          <Dropdown.Item>Settings</Dropdown.Item>
-          <Dropdown.Item>Earnings</Dropdown.Item>
-          <Dropdown.Divider />
-          <Dropdown.Item>Sign out</Dropdown.Item>
+          <Dropdown.Item>My Github</Dropdown.Item>
+          <Dropdown.Item>My Portfolio</Dropdown.Item>
+          <Dropdown.Item>My Tiktok (Coming Soon)</Dropdown.Item>
         </Dropdown>
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
-        <Navbar.Link href="#" active>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive
+              ? "nav-link-active text-blue-600"
+              : "nav-link text-gray-600"
+          }
+        >
           Home
-        </Navbar.Link>
-        <Navbar.Link href="#">About</Navbar.Link>
-        <Navbar.Link href="#">Services</Navbar.Link>
-        <Navbar.Link href="#">Pricing</Navbar.Link>
-        <Navbar.Link href="#">Contact</Navbar.Link>
+        </NavLink>
+        <NavLink
+          to="/documents"
+          className={({ isActive }) =>
+            isActive
+              ? "nav-link-active text-blue-600"
+              : "nav-link text-gray-600"
+          }
+        >
+          List
+        </NavLink>
+        <Navbar.Link href="#">Contoh Slide</Navbar.Link>
+        <Navbar.Link href="#">Contoh Nota</Navbar.Link>
+        <Navbar.Link href="#">About me</Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
   );
