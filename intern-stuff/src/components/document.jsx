531a609b1eb6a379a245ba280cb36ccc3f39973a
YOLO-GIT <ahmadsufi345@gmail.com>
2024-07-22 19:36:04 +0800

    Added new Syntax

--- intern-stuff/src/components/document.jsx
+++ intern-stuff/src/components/document.jsx
@@ -1,31 +1,20 @@
+import Image from "../assets/img-template.png";
 import React from "react";
 import Header from "./header";
 import Socials from "./socials";
 import Footer from "./footer";
+import New_Card from "./tools/card";
+import { BentoGridDemo } from "./tools/bento";
 
-const Documents = () => {
+const Document = () => {
   return (
     <>
       <Header />
-      <section className="bg-white dark:bg-slate-500 min-h-screen p-8">
-        <div className="max-w-screen-lg mx-auto">
-          <h1 className="text-4xl font-extrabold mb-4 dark:text-white text-gray-900">
-            Intern Documents
-          </h1>
-          <ul className="list-disc pl-5 space-y-2 dark:text-gray-200 text-gray-800">
-            <li>Document 1: Description or link</li>
-            <li>Document 2: Description or link</li>
-            <li>Document 3: Description or link</li>
-            <li>Document 4: Description or link</li>
-            <li>Document 5: Description or link</li>
-          </ul>
-        </div>
-      </section>
-
+      <BentoGridDemo />
       <Socials />
       <Footer />
     </>
   );
 };
 
-export default Documents;
+export default Document;
