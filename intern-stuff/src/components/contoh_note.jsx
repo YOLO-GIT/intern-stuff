770021a8d5d23910005e9056443326497376a2a4
YOLO-GIT <ahmadsufi345@gmail.com>
2024-07-25 00:49:04 +0800

    Added "Contoh Note"

---
+++ intern-stuff/src/components/contoh_note.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./header";
+import Image from "../assets/img-template.png";
+import Socials from "./socials";
+import Footer from "./footer";
+import Pin from "./tools/pin";
+
+const Note = () => {
+  return (
+    <>
+      <Header />
+      <Pin />
+      <Socials />
+      <Footer />
+    </>
+  );
+};
+
+export default Note;
