ca7d4509b00b8e42da47547fbeae82bb798192a4
YOLO-GIT <ahmadsufi345@gmail.com>
2024-07-28 00:57:09 +0800

    Added "About Us" section

--- intern-stuff/src/App.jsx
+++ intern-stuff/src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./components/home.jsx";
 import Documents from "./components/document.jsx";
 import Slide from "./components/contoh_slide.jsx";
 import Note from "./components/contoh_note.jsx";
+import About from "./components/about-us.jsx";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/documents" element={<Documents />} />
         <Route path="/contoh_slide" element={<Slide />} />
         <Route path="/contoh_nota" element={<Note />} />
+        <Route path="/about_us" element={<About />} />
       </Routes>
     </Router>
   );
