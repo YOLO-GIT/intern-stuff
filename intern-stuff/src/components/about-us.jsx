d7d977e1e43234b4af9af39fa4c7166330d136b1
YOLO-GIT <ahmadsufi345@gmail.com>
2024-07-28 00:57:19 +0800

    Added new page

---
+++ intern-stuff/src/components/about-us.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./header";
+import Image from "../assets/img-template.png";
+import Socials from "./socials";
+import Footer from "./footer";
+import { BackgroundBoxesDemo } from "./tools/bg-boxes";
+
+const About = () => {
+  return (
+    <>
+      <Header />
+
+      <Socials />
+      <Footer />
+    </>
+  );
+};
+
+export default About;
