7dcd7980ca50e9fbdcbbf0f1332b3f63478f5b70
YOLO-GIT <ahmadsufi345@gmail.com>
2024-07-30 02:03:41 +0800

    Adjust About Us Section

--- intern-stuff/src/components/about-us.jsx
+++ intern-stuff/src/components/about-us.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import Header from "./header";
-import Image from "../assets/img-template.png";
+import Statement from "./statement";
 import Socials from "./socials";
 import Footer from "./footer";
-import { BackgroundBoxesDemo } from "./tools/bg-boxes";
 
 const About = () => {
   return (
     <>
       <Header />
-
+      <Statement />
       <Socials />
       <Footer />
     </>
