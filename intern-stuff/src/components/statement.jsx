89665ca2e3a1e89cb3016ab02391d0361cd54eda
YOLO-GIT <ahmadsufi345@gmail.com>
2024-08-02 01:26:43 +0800

    Modify some lines

--- intern-stuff/src/components/statement.jsx
+++ intern-stuff/src/components/statement.jsx
@@ -9,8 +9,7 @@ import { CardHoverEffect } from "./tools/card-hover";
 const Statement = () => {
   return (
     <>
-      <Header />
-      <section class="bg-white dark:bg-slate-500">
+      <section class="bg-slate-50">
         <div class="grid max-w-screen-lg px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
           <div class="ml-auto place-self-center lg:col-span-7 mt-5 sm:mt-0 rounded-md p-6">
             <h1 class="text-center sm:text-left max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white text-gray-900 sm:text-gray-900">
@@ -34,9 +33,11 @@ const Statement = () => {
         </div>
       </section>
 
-      <CardHoverEffect />
+      <section className="bg-black">
+        <CardHoverEffect />
+      </section>
 
-      <section class="bg-black">
+      <section class="bg-slate-50">
         <div class="container mx-auto px-4 py-8 lg:py-16">
           <div class="grid grid-cols-1 lg:grid-cols-2 gap-20">
             <div class="w-full">
@@ -47,19 +48,16 @@ const Statement = () => {
               />
             </div>
             <div class="flex flex-col justify-center mt-5 sm:mt-0">
-              <h1 class="text-center sm:text-right max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-gray-300">
+              <h1 class="text-center sm:text-right max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-black">
                 Ingin support saya?
               </h1>
-              <p class="text-center sm:text-right mt-6 font-semibold text-gray-200 md:text-lg max-w-lg">
+              <p class="text-center sm:text-right mt-6 font-semibold text-black md:text-lg max-w-lg">
                 Give me money 🤑
               </p>
             </div>
           </div>
         </div>
       </section>
-
-      <Socials />
-      <Footer />
     </>
   );
 };
