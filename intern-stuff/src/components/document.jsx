c890529bce3441f65397c1a6da79f96fe04c5a59
YOLO-GIT <ahmadsufi345@gmail.com>
2024-08-05 01:22:20 +0800

    Fixes some Line

--- intern-stuff/src/components/document.jsx
+++ intern-stuff/src/components/document.jsx
@@ -3,14 +3,14 @@ import React from "react";
 import Header from "./header";
 import Socials from "./socials";
 import Footer from "./footer";
-import { BentoGridDemo } from "./tools/bento";
+import { DocHoverEffect } from "./tools/card-doc";
 
 const Document = () => {
   return (
     <>
       <Header />
-      <div className="bg-slate-50">
-        <BentoGridDemo />
+      <div className="bg-slate-500">
+        <DocHoverEffect />
       </div>
       {/* <Socials /> */}
       <Footer />
