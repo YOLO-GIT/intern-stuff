cc7dcf97eb138e753eb6c79b36576193fced92af
YOLO-GIT <ahmadsufi345@gmail.com>
2024-08-05 01:23:02 +0800

    Duplicate the card-hover UI

---
+++ intern-stuff/src/components/tools/card-doc.jsx
@@ -0,0 +1,52 @@
+import { HoverEffect } from "../ui/card-hover-effect";
+import Intro from "../documents_folder/intro";
+
+export function DocHoverEffect() {
+  return (
+    <div className="max-w-5xl mx-auto px-8">
+      <HoverEffect items={projects} />
+    </div>
+  );
+}
+export const projects = [
+  {
+    title: "Intro",
+    description: "Bahagian pertama untuk budak-udak praktikal.",
+    link: "/documents_folder/intro",
+  },
+  {
+    title: "Bab 1",
+    description: "Objektif Praktikal.",
+    link: "https://netflix.com",
+  },
+  {
+    title: "Bab 2",
+    description: "Sejarah Firma Anda.",
+    link: "https://google.com",
+  },
+  {
+    title: "Bab 3",
+    description: "Daily and Weekly.",
+    link: "https://meta.com",
+  },
+  {
+    title: "Bab 4",
+    description: "Projek yang anda uat untuk firma anda. (Secara asas)",
+    link: "https://amazon.com",
+  },
+  {
+    title: "Bab 5",
+    description: "Kesimpulan yang anda boleh buat selama praktikal anda.",
+    link: "https://microsoft.com",
+  },
+  {
+    title: "Bab 6",
+    description: "Komen dan Candangan.",
+    link: "https://microsoft.com",
+  },
+  {
+    title: "Outro",
+    description: "Reference yang anda boleh beri.",
+    link: "https://microsoft.com",
+  },
+];
