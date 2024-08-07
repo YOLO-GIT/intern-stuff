8137dd68b9a9e96290de259a99775cb313455888
YOLO-GIT <ahmadsufi345@gmail.com>
2024-08-07 23:40:48 +0800

    Added the new Components

--- intern-stuff/src/App.jsx
+++ intern-stuff/src/App.jsx
@@ -7,7 +7,12 @@ import Slide from "./components/contoh_slide.jsx";
 import Note from "./components/contoh_note.jsx";
 import About from "./components/about-us.jsx";
 import Intro from "./components/documents_folder/intro.jsx";
-import bab_1 from "./components/documents_folder/bab_1.jsx";
+import Bab_1 from "./components/documents_folder/bab_1.jsx";
+import Bab_2 from "./components/documents_folder/bab_2.jsx";
+import Bab_3 from "./components/documents_folder/bab_3.jsx";
+import Bab_4 from "./components/documents_folder/bab_4.jsx";
+import Bab_5 from "./components/documents_folder/bab_5.jsx";
+import Bab_6 from "./components/documents_folder/bab_6.jsx";
 
 function App() {
   return (
@@ -19,7 +24,12 @@ function App() {
         <Route path="/contoh_nota" element={<Note />} />
         <Route path="/about_us" element={<About />} />
         <Route path="/documents_folder/intro" element={<Intro />} />
-        <Route path="/documents_folder/bab_1" element={<bab_1 />} />
+        <Route path="/documents_folder/bab_1" element={<Bab_1 />} />
+        <Route path="/documents_folder/bab_2" element={<Bab_2 />} />
+        <Route path="/documents_folder/bab_3" element={<Bab_3 />} />
+        <Route path="/documents_folder/bab_4" element={<Bab_4 />} />
+        <Route path="/documents_folder/bab_5" element={<Bab_5 />} />
+        <Route path="/documents_folder/bab_6" element={<Bab_6 />} />
       </Routes>
     </Router>
   );
