c343c06c6fa9144e1f6f778eb5abc7af07324b0a
YOLO-GIT <ahmadsufi345@gmail.com>
2024-08-11 21:05:18 +0800

    Changes the Background Color

--- intern-stuff/src/components/documents_folder/bab_6.jsx
+++ intern-stuff/src/components/documents_folder/bab_6.jsx
@@ -7,7 +7,7 @@ const bab_6 = () => {
   return (
     <>
       <Header />
-      <section className="bg-white dark:bg-slate-900 w-full h-full">
+      <section className="bg-slate-200 w-full h-full">
         <div className="px-8 py-4 mx-auto">
           <div className="lg:col-span-7 mt-5 sm:mt-0 rounded-md p-14">
             <h1 className="text-center mb-12 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white text-gray-900 sm:text-gray-900">
