88f269ba4aa2a370295a9291c92bba3d3da7468e
YOLO-GIT <ahmadsufi345@gmail.com>
2024-08-13 22:40:01 +0800

    Update the card UI

--- intern-stuff/src/components/tools/card.jsx
+++ intern-stuff/src/components/tools/card.jsx
@@ -1,25 +1,25 @@
 import Image from "../../assets/img-template.png";
 import { CardBody, CardContainer, CardItem } from "../ui/3d-card";
-import { Link } from "react-router-dom";
+import Full_pptx from "../../documents/Slide_Presentation.pptx";
 
 const Card = () => {
   return (
     <CardContainer className="inter-var">
       <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-80 h-auto rounded-xl p-6 border">
         <CardItem
-          translateZ="50"
+          translateZ={50}
           className="text-xl font-bold text-neutral-600 dark:text-white"
         >
-          Make things float in air
+          Contoh Slide Presentation LI
         </CardItem>
         <CardItem
           as="p"
-          translateZ="60"
+          translateZ={60}
           className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
         >
-          Hover over this card to unleash the power of CSS perspective
+          (Efek Hover ini untuk saja - saja.)
         </CardItem>
-        <CardItem translateZ="100" className="w-full mt-4">
+        <CardItem translateZ={100} className="w-full mt-4">
           <img
             src={Image}
             height="1000"
@@ -28,22 +28,15 @@ const Card = () => {
             alt="thumbnail"
           />
         </CardItem>
-        <div className="flex justify-between items-center mt-20">
+        <div className="flex justify-between items-center mt-8">
           <CardItem
             translateZ={20}
-            as={Link}
-            href="https://twitter.com/mannupaaji"
+            as="a"
+            href={Full_pptx}
             target="__blank"
-            className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+            className="px-4 py-2 rounded-xl w-full h-full bg-black dark:bg-white dark:text-black text-white font-bold text-center text-2xl"
           >
-            Try now →
-          </CardItem>
-          <CardItem
-            translateZ={20}
-            as="button"
-            className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
-          >
-            Sign up
+            Tekan Sini
           </CardItem>
         </div>
       </CardBody>
