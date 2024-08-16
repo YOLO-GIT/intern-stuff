45c3e28b15f14085757b4b84c286c81d362354b1
YOLO-GIT <ahmadsufi345@gmail.com>
2024-08-17 00:28:11 +0800

    Update the grid from 2 to 3

--- intern-stuff/src/components/ui/about-hover-doc.jsx
+++ intern-stuff/src/components/ui/about-hover-doc.jsx
@@ -8,7 +8,7 @@ export const AboutEffect = ({ items, className }) => {
   return (
     <div
       className={cn(
-        "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 py-10",
+        "grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 py-10",
         className
       )}
     >
