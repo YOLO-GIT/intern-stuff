6a621a26387b815e63950c86138c2fe5aa3a5497
YOLO-GIT <ahmadsufi345@gmail.com>
2024-08-17 00:27:59 +0800

    Modify the Tools

--- intern-stuff/src/components/tools/card-hover.jsx
+++ intern-stuff/src/components/tools/card-hover.jsx
@@ -9,39 +9,19 @@ export function CardHoverEffect() {
 }
 export const projects = [
   {
-    title: "Stripe",
-    description:
-      "A technology company that builds economic infrastructure for the internet.",
-    link: "https://stripe.com",
+    title: "My Portfolio",
+    description: "My Portfolio 🗣️🔥🔥🔥",
+    link: "https://yolo-git.github.io/My-First-Portfolio/",
   },
   {
-    title: "Netflix",
+    title: "LibraryPro",
     description:
-      "A streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.",
-    link: "https://netflix.com",
+      "Some website I made for some reason 🤷 (Maaf jikalau lambat sedikit nak masuk 😔)",
+    link: "https://libraryprosmktd.000webhostapp.com/",
   },
   {
-    title: "Google",
-    description:
-      "A multinational technology company that specializes in Internet-related services and products.",
-    link: "https://google.com",
-  },
-  {
-    title: "Meta",
-    description:
-      "A technology company that focuses on building products that advance Facebook's mission of bringing the world closer together.",
-    link: "https://meta.com",
-  },
-  {
-    title: "Amazon",
-    description:
-      "A multinational technology company focusing on e-commerce, cloud computing, digital streaming, and artificial intelligence.",
-    link: "https://amazon.com",
-  },
-  {
-    title: "Microsoft",
-    description:
-      "A multinational technology company that develops, manufactures, licenses, supports, and sells computer software, consumer electronics, personal computers, and related services.",
-    link: "https://microsoft.com",
+    title: "Search-Wurd",
+    description: "Just a simple python project",
+    link: "https://github.com/YOLO-GIT/Search-wurd",
   },
 ];
