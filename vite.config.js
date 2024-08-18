6557932235dc94608e77ab83e1f2d31a544d0c52
YOLO-GIT <ahmadsufi345@gmail.com>
2024-08-18 15:51:33 +0800

    Test

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/intern-stuff",
+  base: "/dist",
   plugins: [react()],
   assetsInclude: ["**/*.pptx"],
   build: { chunkSizeWarningLimit: 1600 },
