4e02d7536ea1aeb06a17a8e3fcf9d0a78ca1f483
YOLO-GIT <ahmadsufi345@gmail.com>
2024-08-19 00:41:02 +0800

    Added new Image import

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -5,6 +5,7 @@ import Footer from "./footer";
 import { Typewriter } from "./tools/typewriter";
 import Image_One from "../assets/img_one.gif";
 import Image_Two from "../assets/warning.png";
+import Image_Three from "../assets/tc.jpg";
 
 const Home = () => {
   return (
@@ -65,7 +66,31 @@ const Home = () => {
         </div>
       </section>
 
-      {/* <Socials /> */}
+      <section class="bg-slate-200">
+        <div class="grid max-w-screen-lg px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+          <div class="ml-auto place-self-center lg:col-span-7 mt-5 sm:mt-0 rounded-md p-6">
+            <h1 class="text-center sm:text-left max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white text-gray-900 sm:text-gray-900">
+              Terms & Condition
+            </h1>
+            <p class="text-center sm:text-left mb-6 font-semibold dark:text-white text-gray-800 sm:text-gray-800 md:text-lg">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+          <div class="lg:mt-0 lg:col-span-5 lg:flex mx-auto">
+            <img
+              class="rounded-2xl w-60 sm:w-96 transform transition-transform hover:scale-105 ease-out duration-500 hover:shadow-lg"
+              src={Image_Three}
+              alt="mockup"
+            />
+          </div>
+        </div>
+      </section>
       <Footer />
     </>
   );
