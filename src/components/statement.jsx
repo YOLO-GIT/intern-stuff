5f15e328e8cec73af36ce19e14e7b351f7cccb8b
YOLO-GIT <ahmadsufi345@gmail.com>
2024-08-25 23:13:59 +0800

    Test

--- src/components/statement.jsx
+++ src/components/statement.jsx
@@ -53,7 +53,7 @@ const Statement = () => {
               <p class="text-center sm:text-right mt-6 font-semibold text-black md:text-lg max-w-lg">
                 Give me money 🤑
               </p>
-              <p className="mx-auto sm:ml-auto mt-2">
+              <p className="ml-auto mt-2">
                 <BuyMeACoffeeButton />
               </p>
             </div>
