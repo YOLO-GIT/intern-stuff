d8b1e76ee3c05e6eb952b6037fb15508fa36ef12
YOLO-GIT <ahmadsufi345@gmail.com>
2024-08-28 23:56:43 +0800

    Fixes the link

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,11 @@ import Slide from "./components/contoh_slide.jsx";
 import Note from "./components/contoh_note.jsx";
 import About from "./components/about-us.jsx";
 import Logout from "./components/missing_page/s.jsx";
+import Null_One from "./components/missing_page/c.jsx";
+import Null_Two from "./components/missing_page/a.jsx";
+import Null_Three from "./components/missing_page/r.jsx";
+import Null_Four from "./components/missing_page/e.jsx";
+import Null_Five from "./components/missing_page/questionmark.jsx";
 import Intro from "./components/documents_folder/intro.jsx";
 import Bab_1 from "./components/documents_folder/bab_1.jsx";
 import Bab_2 from "./components/documents_folder/bab_2.jsx";
@@ -26,6 +31,11 @@ function App() {
         <Route path="/contoh_nota" element={<Note />} />
         <Route path="/about_us" element={<About />} />
         <Route path="/logout" element={<Logout />} />
+        <Route path="/null_one" element={<Null_One />} />
+        <Route path="/null_two" element={<Null_Two />} />
+        <Route path="/null_three" element={<Null_Three />} />
+        <Route path="/null_four" element={<Null_Four />} />
+        <Route path="/null_five" element={<Null_Five />} />
         <Route path="/documents_folder/intro" element={<Intro />} />
         <Route path="/documents_folder/bab_1" element={<Bab_1 />} />
         <Route path="/documents_folder/bab_2" element={<Bab_2 />} />
