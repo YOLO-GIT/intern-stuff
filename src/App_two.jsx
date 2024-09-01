aba03b2f562d33053e584fa8f79e99814df31656
YOLO-GIT <ahmadsufi345@gmail.com>
2024-09-02 00:07:18 +0800

    CHange of mind

---
+++ src/App_two.jsx
@@ -0,0 +1,28 @@
+// src/App_two.jsx
+import React from "react";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+
+// Import components for the second app
+import Logout from "./components/missing_page/s.jsx";
+import Null_One from "./components/missing_page/c.jsx";
+import Null_Two from "./components/missing_page/a.jsx";
+import Null_Three from "./components/missing_page/r.jsx";
+import Null_Four from "./components/missing_page/e.jsx";
+import Null_Five from "./components/missing_page/questionmark.jsx";
+
+function App_Two() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Logout />} />
+        <Route path="/null_one" element={<Null_One />} />
+        <Route path="/null_two" element={<Null_Two />} />
+        <Route path="/null_three" element={<Null_Three />} />
+        <Route path="/null_four" element={<Null_Four />} />
+        <Route path="/null_five" element={<Null_Five />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App_Two;
---
+++ src/main_two.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App_two from "./App_two.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root_two")).render(
+  <React.StrictMode>
+    <App_two />
+  </React.StrictMode>
+);
