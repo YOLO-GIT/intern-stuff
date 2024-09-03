dac2e5042541000cfd974a9d17857c01775b2f48
YOLO-GIT <ahmadsufi345@gmail.com>
2024-09-03 23:53:29 +0800

    New components

---
+++ src/components/contoh_lain.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./header";
+import Footer from "./footer";
+import { DisclaimerHighlight } from "./tools/highlight";
+
+const Contoh_lain = () => {
+  return (
+    <>
+      <Header />
+      {/* Disclaimer dan page */}
+      <Footer />
+    </>
+  );
+};
+
+export default Contoh_lain;
